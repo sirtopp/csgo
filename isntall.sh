febfdedb8ce051f7c52aec8192882ad55a5fbe93
tomasz@stadnicki.net <Tom Stadnicki>
2019-10-27 18:47:36 +0100

    Script up to CS:GO game installation

--- isntall.sh
+++ isntall.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 echo "This will install CS:GO Dedicated Server"
+
+if [ -z "${STEAM_SERVER_TOKEN}" ]
+then
+  echo "!!! Missing STEAM_SERVER_TOKEN env variable!"
+  echo "To obtain one go to: https://steamcommunity.com/dev/managegameservers"
+  echo "Set in in calling script using export:"
+  echo
+  echo "export STEAM_SERVER_TOKEN=your-token"
+  echo "./call-this-script"
+  exit 1
+fi
+
+
+apt-get install --no-install-recommends -y lib32gcc1 lib32stdc++6 ca-certificates
+
+useradd -m steam
+mkdir /csgo
+chown steam:steam /csgo
+sudo su - steam
+mkdir ~/Steam && cd ~/Steam
+curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+./steamcmd.sh +login anonymous +force_install_dir /csgo +app_update 740 validate +quit
